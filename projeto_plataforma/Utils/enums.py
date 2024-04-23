f86d938bdf778952c5ec7ddffc084218b782e61b
Lucas <lucasdamata01@hotmail.com>
2024-04-23 08:35:14 -0300

    Up: File enums
    Criado arquivo de "Enums" para referenciar nomes

---
+++ projeto_plataforma/Utils/enums.py
@@ -0,0 +1,7 @@
+class Layers():
+    LAYER_NAME_PLATAFORMS = "Plataformas"
+    LAYER_NAME_ESCADA = "Escada"
+    LAYER_NAME_DECORACAO = "Decoracao"
+    LAYER_NAME_AGUA = "Agua"
+    LAYER_NAME_PLAYER = "Player"
+    LAYER_NAME_ENEMY = "Enemies"
