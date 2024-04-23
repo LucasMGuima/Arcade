845ea189cf706fdb7c4a685d9968d5cb5a674d55
Lucas <lucasdamata01@hotmail.com>
2024-04-23 07:41:03 -0300

    Up: Tempo entre dano recebido

--- projeto_plataforma/Entitys/player.py
+++ projeto_plataforma/Entitys/player.py
@@ -1,6 +1,9 @@
 import Entitys.entity as entity
 
 class Player(entity.Entity):
-    def __init__(self, health=1):
+    def __init__(self, health=1, imortal_time=30):
         file_path = "../assets/Tiles/Characters/tile_0000.png"
         super().__init__(file_path, health)
+
+        self.can_take_damge = True
+        self.imortal_time = imortal_time #60fps
